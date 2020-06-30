1e2e7565af6754b3eea2fb053be2040acd56c9fa
MarGrzy <marcin.grzymowicz94@gmail.com>
2020-06-30 13:50:27 +0200

    Initial commit.

---
+++ README.md
@@ -0,0 +1,12 @@
+# NBA Players Base Application - [BACKEND]
+
+
+## Overview
+1) A web application designed to reviewing information about NBA players.
+
+2) Project is under development
+
+![BasketballLogo](https://cdn.pixabay.com/photo/2013/07/12/14/46/basketball-148766_960_720.png)
+
+### <ins>Author</ins>
+Marcin Grzymowicz
---
+++ src/main/java/pl/mg/projects/players/PlayersBaseApp.java
@@ -0,0 +1,12 @@
+package pl.mg.projects.players;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayersBaseApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlayersBaseApp.class, args);
+    }
+}

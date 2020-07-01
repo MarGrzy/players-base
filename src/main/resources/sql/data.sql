bac3e3a00f56dbd75ca143fa9d02b1686eefc309
MarGrzy <marcin.grzymowicz94@gmail.com>
2020-07-01 16:51:53 +0200

    Data.sql file with example data and JPA/hibernate properties have been added.

---
+++ src/main/resources/sql/data.sql
@@ -0,0 +1,11 @@
+INSERT INTO user (username, password) VALUES ('John', 'john')
+INSERT INTO user (username, password) VALUES ('Doe', 'doe')
+
+INSERT INTO team (id, teamname) VALUE (1, 'Los Angeles Lakers')
+INSERT INTO team (id, teamname) VALUE (2, 'Boston Celtics')
+
+INSERT INTO player (name, position, team_id) VALUES ('LeBron James', 'SF / PF', 1)
+INSERT INTO player (name, position, team_id) VALUES ('Rajon Rondo', 'PG', 1)
+
+INSERT INTO player (name, position, team_id) VALUES ('Jaylen Brown', 'PG / SF', 2)
+INSERT INTO player (name, position, team_id) VALUES ('Kemba Walker', 'PG', 2)

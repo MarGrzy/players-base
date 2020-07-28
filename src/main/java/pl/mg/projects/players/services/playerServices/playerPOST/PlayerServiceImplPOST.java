c3b95a3f25db4a0f4babe1b6a2cd56aac074b1b5
MarGrzy <marcin.grzymowicz94@gmail.com>
2020-07-28 23:08:11 +0200

    POST controller for player has been added with method createNewPlayer.

--- src/main/java/pl/mg/projects/players/services/playerServices/playerPOST/PlayerServiceImplPOST.java
+++ src/main/java/pl/mg/projects/players/services/playerServices/playerPOST/PlayerServiceImplPOST.java
@@ -2,17 +2,23 @@ package pl.mg.projects.players.services.playerServices.playerPOST;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import pl.mg.projects.players.dto.PlayerDto;
 import pl.mg.projects.players.entities.Player;
 import pl.mg.projects.players.entities.Team;
 import pl.mg.projects.players.repositories.PlayerRepository;
+import pl.mg.projects.players.services.mappers.PlayerMapper;
 
 @Service
 public class PlayerServiceImplPOST implements PlayerServicePOST {
 
     private final PlayerRepository playerRepository;
+    private final PlayerMapper playerMapper;
 
     @Autowired
-    public PlayerServiceImplPOST(PlayerRepository playerRepository) { this.playerRepository = playerRepository; }
+    public PlayerServiceImplPOST(PlayerRepository playerRepository, PlayerMapper playerMapper) {
+        this.playerRepository = playerRepository;
+        this.playerMapper = playerMapper;
+    }
 
     @Override
     public void addPlayer(Long id, String playerName, String position, Team team) {
@@ -26,7 +32,8 @@ public class PlayerServiceImplPOST implements PlayerServicePOST {
     }
 
     @Override
-    public void createPlayer(Player newPlayer) {
-        playerRepository.save(newPlayer);
+    public Player createPlayer(PlayerDto newPlayerReceived) {
+        Player newPlayer = playerMapper.toPlayer(newPlayerReceived);
+        return playerRepository.save(newPlayer);
     }
 }

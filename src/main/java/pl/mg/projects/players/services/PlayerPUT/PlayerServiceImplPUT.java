5e2ea659118a6eeef5c193d5ebbcca7f114cb64d
MarGrzy <marcin.grzymowicz94@gmail.com>
2020-07-28 14:17:23 +0200

    Refactored update player method in PlayerServicePUT class (added validation).

--- src/main/java/pl/mg/projects/players/services/PlayerPUT/PlayerServiceImplPUT.java
+++ src/main/java/pl/mg/projects/players/services/PlayerPUT/PlayerServiceImplPUT.java
@@ -2,6 +2,7 @@ package pl.mg.projects.players.services.PlayerPUT;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import pl.mg.projects.players.entities.Player;
+import pl.mg.projects.players.entities.Team;
 import pl.mg.projects.players.repositories.PlayerRepository;
 
 import java.util.Optional;
@@ -17,15 +18,24 @@ public class PlayerServiceImplPUT implements PlayerServicePUT {
 
     @Override
     public boolean updatePlayer(Long playerId, Player updatedPlayer) {
+
+        String newPlayerName = updatedPlayer.getPlayerName();
+        String newPosition = updatedPlayer.getPosition();
+        Team newTeam = updatedPlayer.getTeam();
+
         Optional<Player> playerToUpdate = playerRepository.findById(playerId);
         if (playerToUpdate.isEmpty()) {
             return false;
-        }
-        else
-            playerToUpdate.get().setPlayerName(updatedPlayer.getPlayerName());
-            playerToUpdate.get().setPosition(updatedPlayer.getPosition());
-            playerToUpdate.get().setTeam(updatedPlayer.getTeam());
+        } else {
+            if (!playerToUpdate.get().getPlayerName().equals(newPlayerName)) {
+                playerToUpdate.get().setPlayerName(newPlayerName);
+            } else if (!playerToUpdate.get().getPosition().equals(newPosition)) {
+                playerToUpdate.get().setPosition(newPosition);
+            } else if (!playerToUpdate.get().getTeam().getTeamName().equals(newTeam.getTeamName())){
+                playerToUpdate.get().setTeam(newTeam);
+            }
             playerRepository.save(playerToUpdate.get());
+        }
         return true;
     }
 }

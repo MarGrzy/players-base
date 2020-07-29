37e3fa9f194d2cc9aa730149e3707c59b815093d
MarGrzy <marcin.grzymowicz94@gmail.com>
2020-07-29 15:21:13 +0200

    Refactored updatePlayer method to be more clear.

--- src/main/java/pl/mg/projects/players/services/playerServices/playerPUT/PlayerServiceImplPUT.java
+++ src/main/java/pl/mg/projects/players/services/playerServices/playerPUT/PlayerServiceImplPUT.java
@@ -23,31 +23,33 @@ public class PlayerServiceImplPUT implements PlayerServicePUT {
     @Override
     public void updatePlayer(Long playerId, PlayerDto updatedPlayer) throws PlayerNotFoundException {
 
-        Optional<Player> playerToUpdate = playerRepository.findById(playerId);
+        Optional<Player> optionalPlayerToUpdate = playerRepository.findById(playerId);
+        Player playerToUpdate;
 
         String newPlayerName = updatedPlayer.getPlayerName();
         String newPosition = updatedPlayer.getPosition();
         Team newTeam = updatedPlayer.getTeam();
 
-
-        if (playerToUpdate.isEmpty()) {
+        if (optionalPlayerToUpdate.isEmpty()) {
             throw new PlayerNotFoundException("Player not found in database!");
         } else {
+            playerToUpdate = optionalPlayerToUpdate.get();
+
             if (newPlayerName == null) {
-                newPlayerName = playerToUpdate.get().getPlayerName();
-            } do { playerToUpdate.get().setPlayerName(newPlayerName); }
-            while ((!playerToUpdate.get().getPlayerName().equals(newPlayerName)));
+                newPlayerName = playerToUpdate.getPlayerName();
+            } do { playerToUpdate.setPlayerName(newPlayerName); }
+            while ((!playerToUpdate.getPlayerName().equals(newPlayerName)));
 
             if (newPosition == null) {
-                newPosition = playerToUpdate.get().getPosition();
-            } do { playerToUpdate.get().setPosition(newPosition); }
-            while (!playerToUpdate.get().getPosition().equals(newPosition));
+                newPosition = playerToUpdate.getPosition();
+            } do { playerToUpdate.setPosition(newPosition); }
+            while (!playerToUpdate.getPosition().equals(newPosition));
 
             if (newTeam == null) {
-                newTeam = playerToUpdate.get().getTeam();
-            } do { playerToUpdate.get().setTeam(newTeam); }
-            while (!playerToUpdate.get().getTeam().getTeamName().equals((newTeam).getTeamName()));
+                newTeam = playerToUpdate.getTeam();
+            } do { playerToUpdate.setTeam(newTeam); }
+            while (!playerToUpdate.getTeam().getTeamName().equals((newTeam).getTeamName()));
         }
-        playerRepository.save(playerToUpdate.get());
+        playerRepository.save(playerToUpdate);
     }
 }

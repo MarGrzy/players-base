b4ca8f0d11162c90f82ab872de625f1484ea16c1
MarGrzy <marcin.grzymowicz94@gmail.com>
2020-07-29 15:07:53 +0200

    DELETE player controller with method deletePlayer have been added. Interdependent playerServiceDELETE has been added.

--- src/main/java/pl/mg/projects/players/services/playerServices/playerDELETE/PlayerServiceDELETE.java
+++ src/main/java/pl/mg/projects/players/services/playerServices/playerDELETE/PlayerServiceDELETE.java
@@ -1,9 +1,8 @@
 package pl.mg.projects.players.services.playerServices.playerDELETE;
 
-import pl.mg.projects.players.dto.PlayerDto;
 import pl.mg.projects.players.services.exceptions.PlayerNotFoundException;
 
 public interface PlayerServiceDELETE {
 
-    boolean delete(Long id, PlayerDto player) throws PlayerNotFoundException;
+    boolean delete(Long id) throws PlayerNotFoundException;
 }
--- src/main/java/pl/mg/projects/players/services/playerServices/playerDELETE/PlayerServiceImplDELETE.java
+++ src/main/java/pl/mg/projects/players/services/playerServices/playerDELETE/PlayerServiceImplDELETE.java
@@ -2,9 +2,7 @@ package pl.mg.projects.players.services.playerServices.playerDELETE;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import pl.mg.projects.players.dto.PlayerDto;
 import pl.mg.projects.players.repositories.PlayerRepository;
-import pl.mg.projects.players.services.exceptions.PlayerNotFoundException;
 
 @Service
 public class PlayerServiceImplDELETE implements PlayerServiceDELETE {
@@ -15,11 +13,11 @@ public class PlayerServiceImplDELETE implements PlayerServiceDELETE {
     public PlayerServiceImplDELETE(PlayerRepository playerRepository) { this.playerRepository = playerRepository; }
 
     @Override
-    public boolean delete(Long id, PlayerDto player) throws PlayerNotFoundException {
+    public boolean delete(Long id) {
         if (playerRepository.existsById(id)) {
             playerRepository.deleteById(id);
             return true;
         }
-        else throw new PlayerNotFoundException("Chosen player not found in the database!");
+        else return false;
     }
 }

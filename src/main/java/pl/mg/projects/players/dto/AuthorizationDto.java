26247af9086ad5464c91c7eda94d2583d5664915
MarGrzy <marcin.grzymowicz94@gmail.com>
2020-07-31 23:01:09 +0200

    Validation (jsr-303) for AuthorizationDto (user get from frontend form) has been added.

--- src/main/java/pl/mg/projects/players/dto/AuthorizationDto.java
+++ src/main/java/pl/mg/projects/players/dto/AuthorizationDto.java
@@ -1,7 +1,19 @@
 package pl.mg.projects.players.dto;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
 public class AuthorizationDto {
+
+    @NotNull(message = "{AuthorizationDto.username.notNull}")
+    @Size(min = 3, max = 20, message = "{AuthorizationDto.username.size}")
+    @Pattern(regexp = "(^[A-Z][a-z]*)", message = "{AuthorizationDto.username.pattern}")
     private String username;
+
+    @NotNull(message = "{AuthorizationDto.password.notNull}")
+    @Size(min = 5, max = 20, message = "{AuthorizationDto.password.size}")
+    @Pattern(regexp = "(?=.*\\d)", message = "{AuthorizationDto.password.pattern}")
     private String password;
 
     public AuthorizationDto() {
